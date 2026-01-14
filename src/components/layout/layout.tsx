700b1ee2f6c55ec1b6e601b0ebe958dae61c32bd
lsantander <lsantander@hcdn.gob.ar>
2026-01-14 16:09:21 -0300

    fix: bug visual en animaciones

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Outlet, useLocation } from "react-router-dom"
-import { AnimatePresence, motion } from "framer-motion"
+import { motion } from "framer-motion"
 
 import Header from "./header/header"
 import Footer from "./footer"
@@ -16,24 +16,21 @@ function Layout() {
 
       <Header />
 
-      {/* PAGE TRANSITIONS */}
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={location.pathname}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{
-            duration: 0.25,
-            ease: "easeInOut",
-          }}
-          className="grow"
-        >
-          <main className="container mx-auto px-4 py-6">
-            <Outlet />
-          </main>
-        </motion.div>
-      </AnimatePresence>
+		<motion.div
+			key={location.pathname}
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			transition={{
+			duration: 0.25,
+			ease: "easeInOut",
+			}}
+			className="grow"
+		>
+			<main className="container mx-auto px-4 py-6">
+			<Outlet />
+			</main>
+		</motion.div>
 
       <Footer />
     </div>
